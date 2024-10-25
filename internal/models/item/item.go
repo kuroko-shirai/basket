3a064a1e3d7944c2432b39b2dc02ea921bff1cad
@a.semyannikov_automacon.ru <a.semyannikov@automacon.ru>
2024-10-25 19:03:39 +0300

    Fix Item struct

--- internal/models/item/item.go
+++ internal/models/item/item.go
@@ -3,13 +3,13 @@ package item
 import "time"
 
 type Item struct {
-	Birth   int64
-	Element any
+	Timestamp int64
+	Element   any
 }
 
 func New(element any) *Item {
 	return &Item{
-		Element: element,
-		Birth:   time.Now().Unix(),
+		Element:   element,
+		Timestamp: time.Now().Unix(),
 	}
 }
